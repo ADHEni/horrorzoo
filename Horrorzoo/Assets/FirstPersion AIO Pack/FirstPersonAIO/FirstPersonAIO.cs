e2ee4d698df8e72d6570f04ffc0863cc61790001
ADHEni <enizeithd@gmail.com>
2021-11-01 22:19:55 +0100

    Krasses Update

--- Horrorzoo/Assets/FirstPersion AIO Pack/FirstPersonAIO/FirstPersonAIO.cs
+++ Horrorzoo/Assets/FirstPersion AIO Pack/FirstPersonAIO/FirstPersonAIO.cs
@@ -58,6 +58,7 @@ using UnityEngine.UI;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 
 #if UNITY_EDITOR
     using UnityEditor;
@@ -73,6 +74,7 @@ public class FirstPersonAIO : MonoBehaviour {
 
     #region Input Settings
     public bool controllerPauseState = false;
+    PhotonView view;
     #endregion
 
     #region Look Settings
@@ -243,7 +245,10 @@ public class FirstPersonAIO : MonoBehaviour {
 
     #endregion
 
+
+
     #endregion
+
 
     private void Awake(){
         #region Look Settings - Awake
@@ -332,69 +337,85 @@ public class FirstPersonAIO : MonoBehaviour {
         previousPosition = fps_Rigidbody.position;
         audioSource = GetComponent<AudioSource>();
         #endregion
+
+        view = GetComponent<PhotonView>();
     }
 
     private void Update(){
 
-        #region Look Settings - Update
+        if (view.IsMine)
+        {
 
-            if(enableCameraMovement && !controllerPauseState){
-            float mouseYInput = 0;
-            float mouseXInput = 0;
-            float camFOV = playerCamera.fieldOfView;
-            if (cameraInputMethod == CameraInputMethod.Traditional || cameraInputMethod == CameraInputMethod.TraditionalWithConstraints){
+            #region Look Settings - Update
+
+            if (enableCameraMovement && !controllerPauseState)
+            {
+                float mouseYInput = 0;
+                float mouseXInput = 0;
+                float camFOV = playerCamera.fieldOfView;
+                if (cameraInputMethod == CameraInputMethod.Traditional || cameraInputMethod == CameraInputMethod.TraditionalWithConstraints)
+                {
                     mouseYInput = mouseInputInversion == InvertMouseInput.None || mouseInputInversion == InvertMouseInput.X ? Input.GetAxis("Mouse Y") : -Input.GetAxis("Mouse Y");
                     mouseXInput = mouseInputInversion == InvertMouseInput.None || mouseInputInversion == InvertMouseInput.Y ? Input.GetAxis("Mouse X") : -Input.GetAxis("Mouse X");
+                }
+                else
+                {
+                    mouseXInput = Input.GetAxis("Horizontal") * (mouseInputInversion == InvertMouseInput.None || mouseInputInversion == InvertMouseInput.Y ? 1 : -1);
+                }
+                if (targetAngles.y > 180) { targetAngles.y -= 360; followAngles.y -= 360; } else if (targetAngles.y < -180) { targetAngles.y += 360; followAngles.y += 360; }
+                if (targetAngles.x > 180) { targetAngles.x -= 360; followAngles.x -= 360; } else if (targetAngles.x < -180) { targetAngles.x += 360; followAngles.x += 360; }
+                targetAngles.y += mouseXInput * (mouseSensitivity - ((baseCamFOV - camFOV) * fOVToMouseSensitivity) / 6f);
+                if (cameraInputMethod == CameraInputMethod.Traditional) { targetAngles.x += mouseYInput * (mouseSensitivity - ((baseCamFOV - camFOV) * fOVToMouseSensitivity) / 6f); }
+                else { targetAngles.x = 0f; }
+                targetAngles.x = Mathf.Clamp(targetAngles.x, -0.5f * verticalRotationRange, 0.5f * verticalRotationRange);
+                followAngles = Vector3.SmoothDamp(followAngles, targetAngles, ref followVelocity, (cameraSmoothing) / 100);
+
+                playerCamera.transform.localRotation = Quaternion.Euler(-followAngles.x + originalRotation.x, 0, 0);
+                transform.localRotation = Quaternion.Euler(0, followAngles.y + originalRotation.y, 0);
             }
-            else{
-                mouseXInput= Input.GetAxis("Horizontal") * (mouseInputInversion == InvertMouseInput.None || mouseInputInversion == InvertMouseInput.Y ? 1 : -1);
-            }            if(targetAngles.y > 180) { targetAngles.y -= 360; followAngles.y -= 360; } else if(targetAngles.y < -180) { targetAngles.y += 360; followAngles.y += 360; }
-            if(targetAngles.x > 180) { targetAngles.x -= 360; followAngles.x -= 360; } else if(targetAngles.x < -180) { targetAngles.x += 360; followAngles.x += 360; }
-            targetAngles.y += mouseXInput * (mouseSensitivity - ((baseCamFOV-camFOV)*fOVToMouseSensitivity)/6f);
-            if (cameraInputMethod == CameraInputMethod.Traditional){ targetAngles.x += mouseYInput * (mouseSensitivity - ((baseCamFOV - camFOV) * fOVToMouseSensitivity) / 6f);}
-            else {targetAngles.x = 0f;}
-            targetAngles.x = Mathf.Clamp(targetAngles.x, -0.5f * verticalRotationRange, 0.5f * verticalRotationRange);
-            followAngles = Vector3.SmoothDamp(followAngles, targetAngles, ref followVelocity, (cameraSmoothing)/100);
-            
-            playerCamera.transform.localRotation = Quaternion.Euler(-followAngles.x + originalRotation.x,0,0);
-            transform.localRotation =  Quaternion.Euler(0, followAngles.y+originalRotation.y, 0);
+
+            #endregion
+
+            #region Input Settings - Update
+            if (canHoldJump ? (canJump && Input.GetButton("Jump")) : (Input.GetButtonDown("Jump") && canJump))
+            {
+                jumpInput = true;
+            }
+            else if (Input.GetButtonUp("Jump")) { jumpInput = false; }
+
+
+            if (_crouchModifiers.useCrouch)
+            {
+                if (!_crouchModifiers.toggleCrouch) { isCrouching = _crouchModifiers.crouchOverride || Input.GetKey(_crouchModifiers.crouchKey); }
+                else if (Input.GetKeyDown(_crouchModifiers.crouchKey)) { isCrouching = !isCrouching || _crouchModifiers.crouchOverride; }
+            }
+
+            if (Input.GetButtonDown("Cancel")) { ControllerPause(); }
+            #endregion
+
+            #region Movement Settings - Update
+
+            #endregion
+
+            #region Headbobbing Settings - Update
+
+            #endregion
         }
-    
-        #endregion
-
-        #region Input Settings - Update
-        if(canHoldJump ? (canJump && Input.GetButton("Jump")) : (Input.GetButtonDown("Jump") && canJump) ){
-            jumpInput = true;
-        }else if(Input.GetButtonUp("Jump")){jumpInput = false;}
-        
-        
-        if(_crouchModifiers.useCrouch){
-            if(!_crouchModifiers.toggleCrouch){ isCrouching = _crouchModifiers.crouchOverride || Input.GetKey(_crouchModifiers.crouchKey);}
-            else if(Input.GetKeyDown(_crouchModifiers.crouchKey)){isCrouching = !isCrouching || _crouchModifiers.crouchOverride;}
-            }
-
-        if(Input.GetButtonDown("Cancel")){ControllerPause();}
-        #endregion
-
-        #region Movement Settings - Update
-        
-        #endregion
-
-        #region Headbobbing Settings - Update
-
-        #endregion
 
     }
 
     private void FixedUpdate(){
 
-        #region Look Settings - FixedUpdate
+        if (view.IsMine) 
+        { 
 
-        #endregion
+            #region Look Settings - FixedUpdate
 
-        #region Movement Settings - FixedUpdate
-        
-        if(useStamina){
+            #endregion
+
+            #region Movement Settings - FixedUpdate
+
+            if (useStamina){
             isSprinting = Input.GetKey(sprintKey) && !isCrouching && staminaInternal > 0 && (Mathf.Abs(fps_Rigidbody.velocity.x) > 0.01f || Mathf.Abs(fps_Rigidbody.velocity.z) > 0.01f);
             if(isSprinting){
                 staminaInternal -= (staminaDepletionSpeed*2)*Time.deltaTime;
@@ -716,7 +737,9 @@ public class FirstPersonAIO : MonoBehaviour {
             advanced.isTouchingFlat = false;
         }
         #endregion
-    }
+
+        }
+}
 
  
 
